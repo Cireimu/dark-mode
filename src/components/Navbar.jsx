b6c4e94f775950c38eb2dec4b7db64816125e8dc
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-03-23 10:00:25 -0700

    Revert "Revert "Revert "MVP"""
    This reverts commit 1f443301e89521d7758e9caee3e1cd88715ad044.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 
-import useDarkMode from '../components/hooks/useDarkMode'
-
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode();
+  const [darkMode, setDarkMode] = useState(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
